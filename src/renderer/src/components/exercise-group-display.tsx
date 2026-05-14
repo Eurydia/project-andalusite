e8c99fc6d5c9ab08b29ca591971c6b7615c1a682
Thanakorn Phuttharaksa <crossdeltavi@gmail.com>
2026-05-15 06:47:58 +0700

    Migrate to pnpm; add UI components & views

---
+++ src/renderer/src/components/exercise-group-display.tsx
@@ -0,0 +1,26 @@
+import { Grid } from '@mui/material'
+import { FC } from 'react'
+import { ExercisePreviewCard } from './exercise-preview-card'
+
+export const ExerciseGroupDisplay: FC<{ difficulty: 'ADVANCED' | 'BASIC' | 'INTERMEDIATE' }> = (
+  props
+) => (
+  <Grid container spacing={4}>
+    {Array.from({ length: 3 }).map((_, i) => (
+      <Grid size={4} key={i}>
+        <ExercisePreviewCard
+          exerciseId={'2e8e83e9-fcf2-40d2-aed0-dbbdf14b7704'}
+          name={'EXERCISE ' + props.difficulty + i.toString().padStart(2, '0')}
+          explanation={
+            'lorem pharetra. Quisque volutpat facilisis pharetra. Donec eleifend mauris et tellus mattis dignissim in vehicula turpis. Morbi eu augue eget sapien aliquet lacinia eget tincidunt odio. Etiam malesuada rhoncus cursus. Suspendisse tempor bibendum hendrerit. Quisque interdum ligula et luctus fermentum. Sed molestie porta tortor, ut ullamcorper leo iaculis eget.  '
+          }
+          thumbnailSrc={'https://placehold.co/600x400'}
+          videoClipSrc={'https://www.youtube.com/embed/BPK9WNtpBgk'}
+          difficulty={props.difficulty}
+          approxDurationSeconds={120}
+          tags={'Quisque volutpat facilisis pharetra'.split(' ')}
+        />
+      </Grid>
+    ))}
+  </Grid>
+)
--- src/renderer/src/routes/index.tsx
+++ src/renderer/src/routes/index.tsx
@@ -1,90 +1,22 @@
-import { HelpRounded, SearchRounded } from '@mui/icons-material'
-import {
-  AppBar,
-  Container,
-  Grid,
-  InputAdornment,
-  Stack,
-  TextField,
-  Toolbar,
-  Tooltip,
-  Typography
-} from '@mui/material'
-import { ExercisePreviewCard } from '@renderer/components/exercise-preview-card'
+import { View$MainWindow } from '@renderer/views/media-player-window.view'
 import { createFileRoute } from '@tanstack/react-router'
-import { FC } from 'react'
+import { default as ReactPlayer } from 'react-player'
+import z from 'zod'
 
 export const Route = createFileRoute('/')({
-  component: RouteComponent
+  component: RouteComponent,
+  validateSearch: z.object({
+    view: z.enum(['MAIN', 'MEDIA_PLAYER'])
+  })
 })
 
-const Board: FC<{ difficulty: 'ADVANCED' | 'BASIC' | 'INTERMEDIATE' }> = (props) => (
-  <Grid container spacing={4}>
-    {Array.from({ length: 3 }).map((_, i) => (
-      <Grid size={4} key={i}>
-        <ExercisePreviewCard
-          exerciseId={'2e8e83e9-fcf2-40d2-aed0-dbbdf14b7704'}
-          name={'EXERCISE ' + props.difficulty + i.toString().padStart(2, '0')}
-          explanation={
-            'lorem pharetra. Quisque volutpat facilisis pharetra. Donec eleifend mauris et tellus mattis dignissim in vehicula turpis. Morbi eu augue eget sapien aliquet lacinia eget tincidunt odio. Etiam malesuada rhoncus cursus. Suspendisse tempor bibendum hendrerit. Quisque interdum ligula et luctus fermentum. Sed molestie porta tortor, ut ullamcorper leo iaculis eget.  '
-          }
-          thumbnailSrc={'https://placehold.co/600x400'}
-          videoClipSrc={'https://www.youtube.com/embed/BPK9WNtpBgk'}
-          difficulty={props.difficulty}
-          approxDurationSeconds={120}
-          tags={'Quisque volutpat facilisis pharetra'.split(' ')}
-        />
-      </Grid>
-    ))}
-  </Grid>
-)
-
 function RouteComponent() {
-  return (
-    <>
-      <AppBar color="default" position="sticky">
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
-          <Typography>{`Information`}</Typography>
-          <Typography>{`Exercise poses`}</Typography>
-          <Typography>{`Settings`}</Typography>
-        </Toolbar>
-      </AppBar>
-      <Container maxWidth="lg">
-        <Stack spacing={4} sx={{ paddingY: 4 }}>
-          <Toolbar disableGutters>
-            <TextField
-              fullWidth
-              placeholder="Search exercises"
-              slotProps={{
-                input: {
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <SearchRounded />
-                    </InputAdornment>
-                  ),
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Tooltip
-                        title={
-                          <Typography>{`Search exerices using tags, name, difficulty or duration.`}</Typography>
-                        }
-                      >
-                        <HelpRounded sx={{ cursor: 'pointer' }} />
-                      </Tooltip>
-                    </InputAdornment>
-                  )
-                }
-              }}
-            />
-          </Toolbar>
-          <Typography sx={{ fontWeight: 900 }} variant="h2">{`Basic`}</Typography>
-          <Board difficulty="BASIC" />
-          <Typography sx={{ fontWeight: 900 }} variant="h2">{`Intermediate`}</Typography>
-          <Board difficulty="INTERMEDIATE" />
-          <Typography sx={{ fontWeight: 900 }} variant="h2">{`Advanced`}</Typography>
-          <Board difficulty="ADVANCED" />
-        </Stack>
-      </Container>
-    </>
-  )
+  const search = Route.useSearch()
+  switch (search.view) {
+    case 'MEDIA_PLAYER':
+      return <ReactPlayer />
+    case 'MAIN':
+    default:
+      return <View$MainWindow />
+  }
 }
